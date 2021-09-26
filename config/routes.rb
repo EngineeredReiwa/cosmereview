6cb4b4b79f79c5bcf8926693ed5faab901c2d900
EngineeredReiwa <k.keisuke.1230@gmail.com>
2021-09-26 03:54:36 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #Reviews画面
+  root to: 'reviews#index'
+  resources :reviews
+  
+  #Login/Logout画面
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  #Signin画面
+  get 'signup', to: 'users#new'
+  
 end
