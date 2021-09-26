4de2b0aaabf5c67b3302a4d9499277dc1d73bceb
EngineeredReiwa <k.keisuke.1230@gmail.com>
2021-09-26 04:29:34 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #Reviews画面
   root to: 'reviews#index'
-  resources :reviews
+  resources :reviews, only: [:index, :show, :new, :create, :destroy]
   
   #Login/Logout画面
   get 'login', to: 'sessions#new'
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   
   #Signin画面
   get 'signup', to: 'users#new'
+  
+  #Users画面
+  resources :users, only: [:index, :show, :create]
   
 end
